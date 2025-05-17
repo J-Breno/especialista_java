7d4c1acc01075648ea0d58619d2dfc58df5dc911
J-Breno <joao.breno85@hotmail.com>
2025-05-17 05:46:44 -0300

    TrOrdenando arrays em ordem natural e reversa

--- 03-arrays/arrays/src/Main.java
+++ 03-arrays/arrays/src/Main.java
@@ -1,15 +1,20 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        int[] notas  = new int[5];
-        int[] notas1 = new int[]{8, 5, 4, 9, 10};
+        Integer[] notas1 = new Integer[]{8, 5, 4, 9, 10};
         int[] notas2 = {8, 3, 9, 1, 7};
 
         System.out.println(notas1[2]);
 
+        Arrays.sort(notas2);
+        Arrays.sort(notas1, Comparator.reverseOrder());
+
         String notasEmString = Arrays.toString(notas2);
+        System.out.println(notasEmString);
 
         System.out.println(Calculadora.calcularMedia(notas2));
+        System.out.println(Arrays.toString(notas1));
     }
 }
