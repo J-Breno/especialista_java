90254b807f262f09801eaf5a7e123d997d8c2170
J-Breno <joao.breno85@hotmail.com>
2025-05-16 23:12:54 -0300

    ImplSaindo do método com a cláusula return

--- 02-introducao_POO/01_aula-oo/src/Carro.java
+++ 02-introducao_POO/01_aula-oo/src/Carro.java
@@ -7,11 +7,22 @@ public class Carro {
     Pessoa proprietario;
 
     void imprimirResumoDepriacao() {
+
+        if(precoCompra <= 0) {
+            System.out.println("Carro com preço de compra zerado. Não foi possível imprimir resumo de depreciação.");
+            return;
+        }
+
         System.out.printf("Tempo de uso (anos): %d%n", calcularTempoDeUsoEmAnos());
         System.out.printf("Valor de revenda: R$ %6.2f%n", calcularValorRefenda());
     }
 
     double calcularIpva() {
+        int tempoDeUsoEmAnos = calcularTempoDeUsoEmAnos();
+
+        if(tempoDeUsoEmAnos >= 10){
+            return 0;
+        }
         return calcularValorRefenda() * 0.04;
     }
 
