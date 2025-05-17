92eab0dab4be45c7346971b92a25f68abd6c9aa1
J-Breno <joao.breno85@hotmail.com>
2025-05-17 05:32:14 -0300

    Acessando e atribuindo elementos de arrays

--- 03-arrays/arrays/src/Main.java
+++ 03-arrays/arrays/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         int[] notas  = new int[5];
         int[] notas1 = new int[]{8, 5, 4, 9, 10};
         int[] notas2 = {8, 3, 9, 1, 7};
+
+        System.out.println(notas1[2]);
     }
 }
