2fb3b8f14b8a9063b1923e99fa253e1724764633
J-Breno <joao.breno85@hotmail.com>
2025-05-17 00:18:47 -0300

    MMetodos que alteram o estado de objeto recebido como parametro

--- 02-introducao_POO/01_aula-oo/src/PrincipalPrecificacao.java
+++ 02-introducao_POO/01_aula-oo/src/PrincipalPrecificacao.java
@@ -1,10 +1,12 @@
 public class PrincipalPrecificacao {
     public static void main(String[] args) {
+        Produto produto = new Produto();
+        produto.precoCusto = 100;
         double precoCompraFornecedor = 140;
 
         ServicoDePrecificacao servicoDePrecificacao = new ServicoDePrecificacao();
-        servicoDePrecificacao.calcularPrecoVenda(precoCompraFornecedor);
+        servicoDePrecificacao.definirPrecoVenda(produto, 20);
 
-        System.out.printf("Preço: %.2f%n", precoCompraFornecedor);
+        System.out.printf("Preço de venda: %.2f%n", produto.precoVenda);
     }
 }
