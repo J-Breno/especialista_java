d4bcc40bb5b7abd5fb242ffba9a588fc869fd587
J-Breno <joao.breno85@hotmail.com>
2025-05-18 23:08:52 -0300

    encadeamento de chamadas de construtores

--- 04-contrutores_pacotes_visibilidade/construtores/src/Produto.java
+++ 04-contrutores_pacotes_visibilidade/construtores/src/Produto.java
@@ -1,24 +1,25 @@
 import java.util.Objects;
 
 public class Produto {
+    static final int QUANTIDADE_ESTOQUE_INICIAL = 100;
 
     String nome;
     int quantidadeEstoque;
 
     Produto () {
+        this("Sem nome");
     }
 
     Produto(String nome) {
-        Objects.requireNonNull(nome, "Nome é obrigatório");
-        this.nome = nome;
+        this(nome, QUANTIDADE_ESTOQUE_INICIAL);
     }
 
-    Produto(String nome, int quantidadeEstoque) {
+    Produto(String nome, int estoqueInicial) {
         Objects.requireNonNull(nome, "Nome é obrigatório");
-        if(quantidadeEstoque < 0) {
+        if(estoqueInicial < 0) {
             throw new IllegalArgumentException("Estoque inicial não pode ser negativo");
         }
         this.nome = nome;
-        this.quantidadeEstoque = quantidadeEstoque;
+        quantidadeEstoque = estoqueInicial;
     }
 }
