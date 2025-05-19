8f944293ed84f8b1da05a7daba07cc30e1139066
J-Breno <joao.breno85@hotmail.com>
2025-05-19 04:32:27 -0300

    implementando a herança

--- 06-heranca/banco/src/com/github/jbreno/banco/ContaEspecial.java
+++ 06-heranca/banco/src/com/github/jbreno/banco/ContaEspecial.java
@@ -1,53 +1,9 @@
 package com.github.jbreno.banco;
 
-public class ContaEspecial {
-
-    private Titular titular;
-    private int agencia;
-    private int numero;
-    private double saldo;
-
-    private double valorTotalRendimentos;
+public class ContaEspecial extends ContaInvestimento {
 
     private double tarifaMensal;
     private double limiteChequeEspecial;
-
-    public Titular getTitular() {
-        return titular;
-    }
-
-    public void setTitular(Titular titular) {
-        this.titular = titular;
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public double getSaldoDisponivel() {
-        return getSaldo() + getLimiteChequeEspecial();
-    }
-
-
-    public double getValorTotalRendimentos() {
-        return valorTotalRendimentos;
-    }
 
     public double getTarifaMensal() {
         return tarifaMensal;
@@ -65,43 +21,7 @@ public class ContaEspecial {
         this.limiteChequeEspecial = limiteChequeEspecial;
     }
 
-    public void creditarRendimento(double percentualJuros) {
-        double valorRendimentos = getSaldo() * percentualJuros / 100;
-        this.valorTotalRendimentos += valorRendimentos;
-        depositar(valorRendimentos);
-    }
-
-    public void sacar(double valorSaque) {
-        if(valorSaque <= 0) {
-            throw new IllegalArgumentException("Valor saque deve ser maior que 0");
-        }
-
-        if(getSaldoDisponivel() < valorSaque) {
-            throw new RuntimeException("Valor insuficiente para saque");
-        }
-
-        saldo -= valorSaque;
-    }
-
-    public void depositar(double valorDeposito) {
-        if(valorDeposito <= 0) {
-            throw new IllegalArgumentException("Valor de deposito deve ser maior que 0");
-        }
-
-        saldo += valorDeposito;
-    }
-
-    public void imprimirDemonstrativo() {
-        System.out.println();
-        System.out.printf("Agencia: %d\n", getAgencia());
-        System.out.printf("Conta: %d\n", getNumero());
-        System.out.printf("Titular: %s\n", getTitular().getNome());
-        System.out.printf("Saldo: %.2f\n", getSaldo());
-        System.out.printf("Saldo disponivel: %.2f\n", getSaldoDisponivel());
-    }
-
     public void debitarTarifaMensal() {
-            sacar(getTarifaMensal());
-
+        sacar(getTarifaMensal());
     }
 }
--- 06-heranca/banco/src/com/github/jbreno/banco/ContaInvestimento.java
+++ 06-heranca/banco/src/com/github/jbreno/banco/ContaInvestimento.java
@@ -1,77 +1,17 @@
 package com.github.jbreno.banco;
 
-public class ContaInvestimento {
-
-    private Titular titular;
-    private int agencia;
-    private int numero;
-    private double saldo;
+public class ContaInvestimento extends Conta {
 
     private double valorTotalRendimentos;
 
-    public Titular getTitular() {
-        return titular;
-    }
 
-    public void setTitular(Titular titular) {
-        this.titular = titular;
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-   public double getValorTotalRendimentos() {
+    public double getValorTotalRendimentos() {
         return valorTotalRendimentos;
     }
 
-   public void creditarRendimento(double percentualJuros) {
+    public void creditarRendimento(double percentualJuros) {
         double valorRendimentos = getSaldo() * percentualJuros / 100;
         this.valorTotalRendimentos += valorRendimentos;
         depositar(valorRendimentos);
-    }
-
-    public void sacar(double valorSaque) {
-        if(valorSaque <= 0) {
-            throw new IllegalArgumentException("Valor saque deve ser maior que 0");
-        }
-
-        if(getSaldo() < valorSaque) {
-            throw new RuntimeException("Valor insuficiente para saque");
-        }
-
-        saldo -= valorSaque;
-    }
-
-    public void depositar(double valorDeposito) {
-        if(valorDeposito <= 0) {
-            throw new IllegalArgumentException("Valor de deposito deve ser maior que 0");
-        }
-
-        saldo += valorDeposito;
-    }
-
-    public void imprimirDemonstrativo() {
-        System.out.println();
-        System.out.printf("Agencia: %d\n", getAgencia());
-        System.out.printf("Conta: %d\n", getNumero());
-        System.out.printf("Titular: %s\n", getTitular().getNome());
-        System.out.printf("Saldo: %.2f\n", getSaldo());
     }
 }
