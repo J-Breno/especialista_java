dde2f4bf45691c5c22c12abb26226dfeb2080ee3
J-Breno <joao.breno85@hotmail.com>
2025-05-18 22:58:45 -0300

    sobrecarga de construtores

--- 04-contrutores_pacotes_visibilidade/construtores/src/Produto.java
+++ 04-contrutores_pacotes_visibilidade/construtores/src/Produto.java
@@ -3,6 +3,13 @@ public class Produto {
     String nome;
     int quantidadeEstoque;
 
+    Produto () {
+    }
+
+    Produto(String nome) {
+        this.nome = nome;
+    }
+
     Produto(String nome, int quantidadeEstoque) {
         this.nome = nome;
         this.quantidadeEstoque = quantidadeEstoque;
