3128ac157c08a926f26763b8de566b52073e620c
J-Breno <joao.breno85@hotmail.com>
2025-05-18 23:02:46 -0300

    boa prática validar os argumentos dos construtores também

--- 04-contrutores_pacotes_visibilidade/construtores/src/Produto.java
+++ 04-contrutores_pacotes_visibilidade/construtores/src/Produto.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Produto {
 
     String nome;
@@ -7,10 +9,15 @@ public class Produto {
     }
 
     Produto(String nome) {
+        Objects.requireNonNull(nome, "Nome é obrigatório");
         this.nome = nome;
     }
 
     Produto(String nome, int quantidadeEstoque) {
+        Objects.requireNonNull(nome, "Nome é obrigatório");
+        if(quantidadeEstoque < 0) {
+            throw new IllegalArgumentException("Estoque inicial não pode ser negativo");
+        }
         this.nome = nome;
         this.quantidadeEstoque = quantidadeEstoque;
     }
