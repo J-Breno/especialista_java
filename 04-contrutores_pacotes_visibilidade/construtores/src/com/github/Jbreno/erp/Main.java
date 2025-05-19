c115538ca46add81dfe5057b727f971891c5aef7
J-Breno <joao.breno85@hotmail.com>
2025-05-18 23:44:30 -0300

    importando classes de pacotes

---
+++ 04-contrutores_pacotes_visibilidade/construtores/src/com/github/Jbreno/erp/Main.java
@@ -0,0 +1,11 @@
+package com.github.Jbreno.erp;
+
+import com.github.Jbreno.erp.comercial.Pedido;
+import com.github.Jbreno.erp.estoque.Produto;
+
+public class Main {
+    public static void main(String[] args) {
+        Pedido pedido = new Pedido();
+        Produto produto = new Produto();
+    }
+}
